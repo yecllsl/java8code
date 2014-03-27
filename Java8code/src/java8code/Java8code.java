667a9c97977cb537671815e91d979eb13f240370
yecllsl <yecllsl@gmail.com>
2014-03-27 18:09:16 +0800

    增加NetBeans项目支持

---
+++ Java8code/src/java8code/Java8code.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package java8code;
+
+/**
+ *
+ * @author yechunlin
+ */
+public class Java8code {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
